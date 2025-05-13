8b753e5537aaf40def925f6ff1923727a0cc68bf
thanh-office <thanhnv1@apero.vn>
2025-05-13 15:43:07 +0700

    Revert "Build railway"
    This reverts commit 7d9c0495b5439272ceff2f3db01f7af830ffbd7a.

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set default port if not provided
-PORT="${PORT:-5000}"
-echo "Starting server on port: $PORT"
-
-# Start gunicorn
-exec gunicorn --workers=2 --bind="0.0.0.0:$PORT" wsgi:app
